8b5e719f23b0d3babd17198252a9708242b96c8b
Julius Biskopstoe <jbiskopstoe@gmail.com>
2019-04-21 21:55:49 +0100

    removed trailing comma on all collections

--- webpack.config.js
+++ webpack.config.js
@@ -6,40 +6,40 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [".ts", ".tsx", ".js"]
   },
   devtool: "cheap-eval-source-map",
   output: {
     path: path.join(__dirname, "/dist"),
-    filename: "bundle.min.js",
+    filename: "bundle.min.js"
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: [/\.test.tsx?$/, /setupTests.ts/],
-        loader: "awesome-typescript-loader",
+        loader: "awesome-typescript-loader"
       },
       {
         test: /\.s?css$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           "file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]",
-          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false",
-        ],
-      },
-    ],
+          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false"
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
+      template: "./src/index.html"
+    })
   ],
   watchOptions: {
     aggregateTimeout: 300,
-    poll: 1000,
-  },
+    poll: 1000
+  }
 };
